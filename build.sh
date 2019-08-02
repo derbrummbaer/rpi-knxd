c08965079d2aff3a65b143dfd3e919210f25a3a0
Sven Berger <sven@berger-lembeck.de>
2019-08-02 19:28:20 +0200

    Latest Verschoben

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker build -t derbrummbaer/rpi-knxd:jessie  ./jessie
 docker build -t derbrummbaer/rpi-knxd:stretch ./stretch
 docker build -t derbrummbaer/rpi-knxd:buster  ./buster
-docker build -t derbrummbaer/rpi-knxd:latest  .
+docker build -t derbrummbaer/rpi-knxd:latest  ./latest
